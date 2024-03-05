af2abaa3c496b362d56f86ffd3d46ebddb85f606
Antonin Pamart <antonin.pamart@mezcalito.fr>
2024-03-05 17:59:36 +0100

    Init

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
 
-    #[Route('home', 'home.html.twig')]
+    #[Route('/')]
     public function renderHome(): Response
     {
-       return $this->render('home');
+       return $this->render('home.html.twig');
     }
 
 }
