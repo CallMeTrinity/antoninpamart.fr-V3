abaa40a10df414ef4807d05d8295acb1ccc0eef1
Antonin Pamart <antonin.pamart@mezcalito.fr>
2024-03-19 12:29:56 +0100

    feat(MoiManager): Init

---
+++ src/Form/TrinityType.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Pamart_PortfolioV3 project.
+ *
+ * (c) Antonin <contact@antoninpamart.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Moi;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrinityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('aboutMe')
+            ->add('pp')
+            ->add('age')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Moi::class,
+        ]);
+    }
+}
