bc49d9486964f3166a63bdeb4685eb6df4de3ad0
Antonin Pamart <antonin.pamart@mezcalito.fr>
2024-03-28 12:08:51 +0100

    feat(Admin): Admin_trinity reworked plus managed file upload

---
+++ src/Twig/Components/ProfilePictureModal.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the antoninpamart.fr-V3 project.
+ *
+ * (c) Antonin <contact@antoninpamart.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Twig\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class ProfilePictureModal
+{
+    use DefaultActionTrait;
+
+    #[LiveProp]
+    public array $images = [];
+
+    public function getImages()
+    {
+
+    }
+}
